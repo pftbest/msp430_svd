3edb2bd5b5a99730c8ddd27c50cfc31962c182f0
William D. Jones <thor0505@comcast.net>
2023-12-10 00:43:30 -0500

    Automatically format and patch output SVD using svd_encoder and svdtools crates.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate indexmap;
 extern crate svd_parser as svd;
 extern crate xmltree;
 extern crate svd_encoder;
+extern crate svdtools;
 
 #[macro_use]
 mod utils;
@@ -11,9 +12,12 @@ mod dslite_to_svd;
 mod header_parser;
 
 use std::env;
-use std::path::PathBuf;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use svd_encoder::{Config, Encode, NumberFormat};
+use svdtools::patch;
 
 fn main() {
     let mcu_name = match env::args().nth(1) {
@@ -26,9 +30,11 @@ fn main() {
 
     let dslite_root = PathBuf::from("msp430-gcc-support-files/targetdb/devices");
     let headers_root = PathBuf::from("msp430-gcc-support-files/include");
+    let svd_file = format!("{}.svd", mcu_name.to_lowercase());
 
     let dslite_path = dslite_root.join(format!("{}.xml", mcu_name.to_uppercase()));
     let header_path = headers_root.join(format!("{}.h", mcu_name.to_lowercase()));
+    let svd_raw_path = Path::new(&svd_file);
 
     if !dslite_path.is_file() {
         eprintln!("Can't find dslite xml file in {:?}", dslite_path);
@@ -43,6 +49,8 @@ fn main() {
     let dslite_dev = dslite_parser::parse_dslite(&dslite_path);
     let interrupts = header_parser::parse_header(&header_path);
 
+    // This should match config/encoding.json for patched SVD. When svdtools gets
+    // support for directly taking config files, we can remove config/encoding.json.
     let config = Config::default()
         .peripheral_base_address(NumberFormat::Dec)
         .address_block_offset(NumberFormat::Dec)
@@ -57,7 +65,7 @@ fn main() {
         .dim_increment(NumberFormat::Dec);
 
     let svd_dev = dslite_to_svd::build_svd_device(&dslite_dev, &interrupts);
-    let svd_enc = match svd_dev.encode_with_config(&config) {
+    let svd_enc = match svd_encoder::encode_with_config(&svd_dev, &config) {
         Ok(encoded) => encoded,
         Err(e) => {
             eprintln!("Encoding output SVD failed: {}", e);
@@ -65,30 +73,36 @@ fn main() {
         }
     };
 
-    let mut out = Vec::new();
-    match svd_enc.write(&mut out) {
-        Ok(_) => {},
-        Err(e) => {
-            eprintln!("Converting output SVD to Vec failed: {}", e);
-            return;
-        }
-    };
-
-    let out_svd = match String::from_utf8(out) {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("Converting output SVD to String failed: {}", e);
-            return;
-        }
-    };
-
-    match svd::parse(&out_svd) {
+    match svd::parse(&svd_enc) {
         Ok(parsed) => { assert_eq!(svd_dev, parsed); },
         Err(_e) => {
-            eprintln!("Parsing output file {} during round-trip test failed", out_svd);
+            eprintln!("Parsing output file {} during round-trip test failed", svd_enc);
             return;
         }
     }
 
-    println!("{}", out_svd);
+    let mut svd_fp = match File::create(svd_raw_path) {
+        Ok(fp) => fp,
+        Err(e) => {
+            eprintln!("Creating unpatched SVD file failed: {}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = svd_fp.write_all(&svd_enc.as_bytes()) {
+        eprintln!("Writing unpatched SVD file failed: {}", e);
+        return;
+    }
+
+    drop(svd_fp);
+
+    let yaml_devices_root = PathBuf::from("overrides/devices");
+    let dev_yaml_path = yaml_devices_root.join(format!("{}.yaml", mcu_name.to_lowercase()));
+    match patch::process_file(&dev_yaml_path, None, Some(Path::new("config/encoding.json")), &patch::Config::default()) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Patching SVD file failed: {}", e);
+            return;
+        }
+    }
 }
