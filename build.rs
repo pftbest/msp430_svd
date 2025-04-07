04543f91320c72c8b73c3897a384b444c489828b
Robin Mueller <robin.mueller.m@gmail.com>
2025-04-07 13:45:23 +0200

    code review changes

--- build.rs
+++ build.rs
@@ -2,10 +2,11 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     vergen_gitcl::Emitter::default()
-        .add_instructions(&vergen_gitcl::GitclBuilder::all_git()?)?
-        .emit()?;
-    vergen_git2::Emitter::default()
-        .add_instructions(&vergen_git2::Git2Builder::all_git()?)?
+        .add_instructions(
+            &vergen_gitcl::GitclBuilder::default()
+                .describe(true, true, None)
+                .build()?,
+        )?
         .emit()?;
     Ok(())
 }
